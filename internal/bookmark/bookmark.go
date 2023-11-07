de6b6108545f275dfafe0d86aa0bc4835186b2c1
Aaron Johnson <commondatageek@gmail.com>
2023-11-06 20:43:48 -0700

    Add tags and description to bookmark

--- internal/bookmark/bookmark.go
+++ internal/bookmark/bookmark.go
@@ -6,16 +6,20 @@ import (
 )
 
 type Bookmark struct {
-	Names []string `json:"names"`
-	URL   string   `json:"url"`
+	Names       []string `json:"names"`
+	Tags        []string `json:"tags"`
+	URL         string   `json:"url"`
+	Description string   `json:"description"`
 }
 
 func (b Bookmark) String() string {
-	nameStrs := make([]string, len(b.Names))
-	for i := 0; i < len(nameStrs); i++ {
-		nameStrs[i] = fmt.Sprintf(`"%s"`, b.Names[i])
-	}
-	namesString := strings.Join(nameStrs, ", ")
+	namesString := "\"" + strings.Join(b.Names, "\", \"") + "\""
+	tagsString := "\"" + strings.Join(b.Tags, "\", \"") + "\""
 
-	return fmt.Sprintf(`Bookmark(Names: [%s], URL: "%s")`, namesString, b.URL)
+	return fmt.Sprintf(`Bookmark(
+	Names: [%s]
+	Tags: [%s]
+	URL: %s
+	Description: %s
+)`, namesString, tagsString, b.URL, b.Description)
 }
